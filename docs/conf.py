3183d7dbddecff80d33f7f35bbaadaf73610b8bf
Luke <luke@kulora.com>
2016-05-01 03:41:37 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,19 +17,6 @@ import os
 import re
 import sys
 
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# read version number (and other metadata) from package init
-init_fn = os.path.join(here, '..', 'certbot', '__init__.py')
-with codecs.open(init_fn, encoding='utf8') as fd:
-    meta = dict(re.findall(r"""__([a-z]+)__ = '([^']+)""", fd.read()))
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
