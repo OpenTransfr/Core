298d3e352193e574e0c8980e37a50d226552109e
Luke <luke@kulora.com>
2016-05-01 03:30:37 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'repoze.sphinx.autointerface',
+    'sphinxcontrib.programoutput',
+    'sphinxcontrib.images',
+]
